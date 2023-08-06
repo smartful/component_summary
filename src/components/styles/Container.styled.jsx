2fbdb3e2458345bc58d034f63922702b37c4b88a
smartful <remi_rodrigues@hotmail.com>
2023-08-06 05:24:27 +0200

    re-sizing globaly the component

--- src/components/styles/Container.styled.jsx
+++ src/components/styles/Container.styled.jsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   background-color: white;
-  width: 800px;
-  height: 600px;
+  width: 700px;
+  height: 500px;
   box-shadow: 5px 5px 30px rgba(0, 0, 100, 0.2);
   border-radius: 25px;
   display: flex;
--- src/components/styles/ResultDetails.styled.jsx
+++ src/components/styles/ResultDetails.styled.jsx
@@ -3,4 +3,5 @@ import styled from '@emotion/styled';
 export const ResultDetails = styled.div`
   width: 66%;
   margin-top: 20px;
+  text-align: center;
 `;
